2b2023c1c34cbe2e2878a50c96d397a884dac341
acortinovis <adelio.cortinovis@gmail.com>
2025-02-06 12:32:00 -0800

    finished loop game
    finished the loop and tested it

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ tentatives=0
 
 #give the user 6 chances and provide feedbacks
 print ("guess the number between 1 and 50! ")
-if tentative<6:
+while True:
+    if tentatives<6:
 
-    guess = float (input ("write your number ").strip())
-
-    if guess==number:
-        print("congratulation, you guessed the number! ")
+        guess = float (input ("write your number ").strip())
+        tentatives= tentatives+1
+        if guess==number:
+           print("congratulation, you guessed the number! ")
+        else:
+            if guess<number:
+              print("too low, try again")
+            else:
+              print("too high, try again")
     else:
-        if guess<number:
-            print("too low, try again")
+        print("you didn't guess, the number was ",number)
+        break
+
 
